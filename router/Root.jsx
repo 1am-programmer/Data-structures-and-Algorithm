856bc055ee2cee4a80293822e9a7c6cd0b01cf8a
Your Name <dannydotdev@gmail.com>
2024-04-26 11:59:14 +0100

    routing

---
+++ router/Root.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../src/App";
+
+const root = () => {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+};
+
+export default root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./router/Root.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
