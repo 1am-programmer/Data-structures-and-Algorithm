445edf644898adddbb87fdf065332315f5a08198
Your Name <dannydotdev@gmail.com>
2024-04-27 16:47:49 +0100

    Big O/ Quadraatic and Quasilinear runtimes

--- router/BigO.jsx
+++ router/BigO.jsx
@@ -110,7 +110,60 @@ export default function BigO() {
         </p>
         <p>
           Sublinear runtimes are sometimes preferred to, over linear , because
-          it is more efficient, although linear has it's own advantages{" "}
+          it is more efficient, although linear has it's own advantages: For
+          example, when the input is not sorted
+        </p>
+      </div>
+      <div>
+        <h3 className="font-bold py-5 text-center">
+          Quadratic Time represented by O(n2) or say an algorithm with a
+          quadratic runtime
+        </h3>
+        <ul>
+          <li className="list-disc">
+            For any given value of n, we carry out n square (n2) number of
+            operations
+          </li>
+
+          <li className="list-disc">
+            Most search Algorithm have a worst case algorithm runtime
+          </li>
+          <li className="list-disc">
+            In addition to Quadratic runtime, you may run into a cubic runtime
+            as you encounter different Algorithm, here the algorithm executes
+            the n3 or n^3 (n raise to the power of the 3rd number of operations)
+            <span>
+              For a small changes in n, there is a significant change in the
+              number of operations that we need to carry out{" "}
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h3 className="font-bold py-5 text-center">
+          Quasilinear runtime represented as O(n log n ) which means n times
+          logarithm of n
+        </h3>
+        <p></p>
+        <ul>
+          <li className="list-disc">
+            For every value of n, we are going to execute a log n number of
+            operations hence the runtime of n times log n operations
+          </li>
+          <li className="list-disc">
+            A Quasilinear runtime, has a runtime that lies somewhere between
+            linear and quadratic runtimes
+          </li>
+        </ul>
+        <p>Where we can see this runtime in practical use:</p>
+        <p>
+          Sorting Algorithms,{" "}
+          <span>
+            e.g Merge Sort:
+            <br />
+            <span>Has a worse case runtime of O(n log n)</span>
+          </span>
         </p>
       </div>
     </div>
